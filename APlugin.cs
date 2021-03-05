f1335dd2fd35c10e87ad0715d480a3f93385b393
Fahrettin Enes <76036578+fahrettinenes@users.noreply.github.com>
2021-03-05 07:31:01 +0300

    Functions added.

--- APlugin.cs
+++ APlugin.cs
@@ -44,6 +44,17 @@ namespace AtomicLibrary
 			else
 				configuration = new XMLFileAsset<RocketPluginConfiguration>(text, null, default(RocketPluginConfiguration));
 		}
+		
+		public static uint serverIP() => Provider.ip;
+		public static string serverMap() => Provider.map;
+		public static int serverPlayerCount() => Provider.clients.Count;
+		public static int serverPlayerMaxPlayers() => Provider.clients.Capacity;
+		public static string serverPassword() => Provider.serverPassword;
+		public static string serverName() => Provider.serverName;
+		public static string serverID() => Provider.serverID;
+		public static ushort serverPort() => Provider.port;
+
+		public static void ban(CSteamID steamID, string reason, uint duration) => UnturnedPlayer.FromCSteamID(steamID).Ban(reason, duration);
 
 		private IAsset<RocketPluginConfiguration> configuration;
 	}
